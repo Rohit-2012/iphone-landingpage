9acc20c4a6763db891934260c4b1ef885a72b64f
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-15 10:11:40 +0530

    add 3D iPhone model

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
